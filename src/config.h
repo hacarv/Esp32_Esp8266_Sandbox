c32e92be97562e2d91bf72727eabb1c4b20bcb88
Harold Carvajal <hacarv101@gmail.com>
2024-11-18 22:57:55 -0600

    Update default password

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 
 #define DEFAULT_SSID ""
-#define DEFAULT_PASSWORD "1702shd1"
+#define DEFAULT_PASSWORD "12345678"
 #define DEFAULT_AP_SSID "ESP-AP"
 #define DEFAULT_AP_PASSWORD ""
 #define DEFAULT_AP_IP "192.168.4.1"
